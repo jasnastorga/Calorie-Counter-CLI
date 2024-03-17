4400e2decac96f89f02b977b6c3e43b79167f21d
Jason Astorga <jast590@gmail.com>
2024-03-17 03:10:49 +0000

    first commit

---
+++ main.rb
@@ -0,0 +1,16 @@
+class Food
+
+  def initialize(name, type, calories)
+    @name = name
+    @type = type
+    @calories = calories
+
+    puts "Welcome to your Calorie diary!"
+    puts "Please enter food name, type (e.g fruit, veggie,meat), and estimated calories."
+  end
+
+
+
+end
+
+#create an instance of the food class
