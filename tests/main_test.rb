1b6239e097c60a22048153a2145963b3171d849d
Jason Astorga <jast590@gmail.com>
2024-03-18 17:27:34 +0000

    created test folder, fixed test cases

--- tests/main_test.rb
+++ tests/main_test.rb
@@ -1,5 +1,5 @@
 require 'minitest/autorun'
-require_relative 'food'
+require_relative '/workspaces/Calorie-Counter-CLI/food'
 
 #do not include in test file, main.rb contains 'gets' as this will force user input
 #require_relative 'main'
@@ -8,7 +8,7 @@ class TestFood < Minitest::Test
   def test_food_inserting
     entry = Food.new("apple","fruit",150)
     assert_equal "apple", entry.name, "name creation failed"
-    assert_equal "meat", entry.type, "incorrect food type"
+    assert_equal "fruit", entry.type, "incorrect food type"
     assert_equal 150, entry.calories, "calorie creation failed"
   end
 end
